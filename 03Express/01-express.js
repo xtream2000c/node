d59e34eba3ce42d5e843d4a1c41beb7ad97dc102
xtream2000c <carlmarcast@gmail.com>
2022-02-03 16:12:00 +0100

    Añadidos templates, con bootstrap, y diferentes vistas

--- 03Express/01-express.js
+++ 03Express/01-express.js
@@ -3,24 +3,30 @@ const express = require('express') //Requiere express
 const app = express() //Variable para disponer de todas las variables de Express
 const port = 3000 //Puerto de escucha
 
+//Motor de plantillas
 app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname+'/public'));
+//***********************Ruta para ficheros dinamicos*************************
+app.set('views', __dirname+'/views');
 
-app.use('/', (req, res) => {
-  res.status(404).sendFile(__dirname+"/public/404.html") 
-})
+//***********************Ruta para ficheros estaticos*************************
+app.use(express.static(__dirname+'/public'));
 
 //Por peticion de cliente GET
 app.get('/', (req, res) => { //Usamos funcion flecha para evitar funciones innecesarias OBLIGATORIO req y res, '/' es el directorio en que va a buscar (Localhost)
-  res.send('Sto ha cambiao? ahora si') 
+  res.render('index',{titulo:'Este es un titulo dinamico'}) 
 })
 
 app.get('/contacto', (req, res) => { // usamos /contacto para crear una nueva ruta
-    res.send('Estas en contacto') 
-  })
+    res.render('contacto', {tituloContacto:'Estas en contacto'}) 
+})
 
-
+app.use('/', (req, res) => {
+  res.status(404).render("404", {
+    titulo:"Error 404", 
+    descripcion:"Pagina no encontrada"
+  }) 
+})
 
 //Importante mostrar el puerto para cuando este en produccion saber cual es el puerto de escucha
 app.listen(port, () => {
